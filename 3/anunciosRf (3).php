0eae56a7529f9f7cdc8025845b4731d96b57e9ba
Maycko1 <96599672+Maycko1@users.noreply.github.com>
2021-12-31 15:22:08 -0600

    Add files via upload

---
+++ 3/anunciosRf (3).php
@@ -0,0 +1,66 @@
+
+
+
+
+[
+  - {
+
+  		anunciosList: "1",
+  		
+  		fileUrl: "https://firebasestorage.googleapis.com/v0/b/apipredicas.appspot.com/o/plo%202.mp4?alt=media&token=3f52ec9f-08fb-495b-956f-0629979daf1a",
+ 	 	id: 1,
+
+        imageUrl:"https://scontent.fmex13-1.fna.fbcdn.net/v/t1.6435-9/191804465_106159798341439_4258973991311615394_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeE4hZenmsSZVz4loSDjFSL71g36iPLh7W7WDfqI8uHtblGF7VqqlKEHYmxFFVZqYNU93ixcnq7YnurvlZ5wtGZ0&_nc_ohc=C-EL9zXgtjYAX-22I_K&tn=cDe4s_ece1cGv1Z3&_nc_ht=scontent.fmex13-1.fna&oh=0d27ab5da071b607df5959fc1e7cd88a&oe=60D3DC37",
+
+ 	 	movieName: "EJEMPLO 01"
+ 		},
+
+ 	- {
+
+  		anunciosList: "1",
+  		fileUrl: "https://firebasestorage.googleapis.com/v0/b/apipredicas.appspot.com/o/plo%202.mp4?alt=media&token=3f52ec9f-08fb-495b-956f-0629979daf1a", 
+ 	 	id: 2,
+        imageUrl:"https://scontent.fmex13-1.fna.fbcdn.net/v/t1.6435-9/191804465_106159798341439_4258973991311615394_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeE4hZenmsSZVz4loSDjFSL71g36iPLh7W7WDfqI8uHtblGF7VqqlKEHYmxFFVZqYNU93ixcnq7YnurvlZ5wtGZ0&_nc_ohc=C-EL9zXgtjYAX-22I_K&tn=cDe4s_ece1cGv1Z3&_nc_ht=scontent.fmex13-1.fna&oh=0d27ab5da071b607df5959fc1e7cd88a&oe=60D3DC37",
+ 	 	movieName: "EJEMPLO 02"
+ 		},
+
+ 	- {
+
+  		anunciosList: "1",
+  		fileUrl: "https://firebasestorage.googleapis.com/v0/b/apipredicas.appspot.com/o/plo%202.mp4?alt=media&token=3f52ec9f-08fb-495b-956f-0629979daf1a",
+ 	 	id: 3,
+        imageUrl:"https://scontent.fmex13-1.fna.fbcdn.net/v/t1.6435-9/191804465_106159798341439_4258973991311615394_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeE4hZenmsSZVz4loSDjFSL71g36iPLh7W7WDfqI8uHtblGF7VqqlKEHYmxFFVZqYNU93ixcnq7YnurvlZ5wtGZ0&_nc_ohc=C-EL9zXgtjYAX-22I_K&tn=cDe4s_ece1cGv1Z3&_nc_ht=scontent.fmex13-1.fna&oh=0d27ab5da071b607df5959fc1e7cd88a&oe=60D3DC37",
+ 	 	movieName: "EJEMPLO 03"
+ 	},
+
+ - {
+
+  		anunciosList: "2",
+  		fileUrl: "https://firebasestorage.googleapis.com/v0/b/apipredicas.appspot.com/o/plo%202.mp4?alt=media&token=3f52ec9f-08fb-495b-956f-0629979daf1a",
+ 	 	id: 1,
+        imageUrl:"https://scontent.fmex13-1.fna.fbcdn.net/v/t1.6435-9/191804465_106159798341439_4258973991311615394_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeE4hZenmsSZVz4loSDjFSL71g36iPLh7W7WDfqI8uHtblGF7VqqlKEHYmxFFVZqYNU93ixcnq7YnurvlZ5wtGZ0&_nc_ohc=C-EL9zXgtjYAX-22I_K&tn=cDe4s_ece1cGv1Z3&_nc_ht=scontent.fmex13-1.fna&oh=0d27ab5da071b607df5959fc1e7cd88a&oe=60D3DC37",
+ 	 	movieName: "EJEMPLO 01"
+ 	},
+
+
+    - {
+
+  		anunciosList: "2",
+  		fileUrl: "https://firebasestorage.googleapis.com/v0/b/apipredicas.appspot.com/o/plo%202.mp4?alt=media&token=3f52ec9f-08fb-495b-956f-0629979daf1a",
+ 	 	id: 2,
+        imageUrl:"https://scontent.fmex13-1.fna.fbcdn.net/v/t1.6435-9/191804465_106159798341439_4258973991311615394_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeE4hZenmsSZVz4loSDjFSL71g36iPLh7W7WDfqI8uHtblGF7VqqlKEHYmxFFVZqYNU93ixcnq7YnurvlZ5wtGZ0&_nc_ohc=C-EL9zXgtjYAX-22I_K&tn=cDe4s_ece1cGv1Z3&_nc_ht=scontent.fmex13-1.fna&oh=0d27ab5da071b607df5959fc1e7cd88a&oe=60D3DC37",
+ 	 	movieName: "EJEMPLO 01"
+ 	},
+
+    - {
+
+  		anunciosList: "2",
+  		fileUrl: "https://firebasestorage.googleapis.com/v0/b/apipredicas.appspot.com/o/plo%202.mp4?alt=media&token=3f52ec9f-08fb-495b-956f-0629979daf1a",
+ 	 	id: 3,
+        imageUrl:"https://scontent.fmex13-1.fna.fbcdn.net/v/t1.6435-9/191804465_106159798341439_4258973991311615394_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeE4hZenmsSZVz4loSDjFSL71g36iPLh7W7WDfqI8uHtblGF7VqqlKEHYmxFFVZqYNU93ixcnq7YnurvlZ5wtGZ0&_nc_ohc=C-EL9zXgtjYAX-22I_K&tn=cDe4s_ece1cGv1Z3&_nc_ht=scontent.fmex13-1.fna&oh=0d27ab5da071b607df5959fc1e7cd88a&oe=60D3DC37",
+ 	 	movieName: "EJEMPLO 01",
+ 	}
+
+ ]
+
+ 	 
